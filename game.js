1e15797917311ffbb2f0a77e02f02d01762fa2fc
ggponti74 <ggponti74@hotmail.com>
2026-07-18 16:43:11 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// --- Resource Loader ---
+const images = {};
+async function loadResources(assets) {
+    for (const [key, src] of Object.entries(assets)) {
+        await new Promise((resolve) => {
+            const img = new Image();
+            img.onload = () => { images[key] = img; resolve(); };
+            img.src = src;
+        });
+    }
+}
+
+// --- Sprite Animation Class ---
+class Sprite {
+    constructor(image, frameWidth, frameHeight, frameCount) {
+        this.image = image;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.frameCount = frameCount;
+        this.currentFrame = 0;
+        this.timer = 0;
+    }
+    update() {
+        this.timer++;
+        if (this.timer > 10) { // Change frame every 10 ticks
+            this.currentFrame = (this.currentFrame + 1) % this.frameCount;
+            this.timer = 0;
+        }
+    }
+    draw(x, y) {
+        ctx.drawImage(this.image, this.currentFrame * this.frameWidth, 0, 
+                      this.frameWidth, this.frameHeight, x, y, this.frameWidth, this.frameHeight);
+    }
+}
+
+// --- Game Loop ---
+let player;
+async function init() {
+    await loadResources({ hero: 'hero_sprite.png' });
+    player = new Sprite(images.hero, 64, 64, 4);
+    requestAnimationFrame(gameLoop);
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    player.update();
+    player.draw(100, 100);
+    
+    requestAnimationFrame(gameLoop);
+}
+
+init();
